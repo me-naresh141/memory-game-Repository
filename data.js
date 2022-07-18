d7850de6d81ab20c533327abfd62fc936bf47055
me-naresh141 <me.naresh141@gmail.com>
2022-07-18 12:18:44 +0530

    memory-game

---
+++ data.js
@@ -0,0 +1,50 @@
+const cardsArray = [
+  {
+    name: 'shell',
+    img: './assets/ahmed-yanaal-SzX8e6I4au8-unsplash.jpg',
+  },
+  {
+    name: 'star',
+    img: './assets/charlesdeluvio-0v_1TPz1uXw-unsplash.jpg',
+  },
+  {
+    name: 'bobomb',
+    img: './assets/happy-surani-2-nA5KasEoE-unsplash.jpg',
+  },
+  {
+    name: 'mario',
+    img: './assets/jacek-dylag-kH3Sr9K8EBA-unsplash.jpg',
+  },
+  {
+    name: 'luigi',
+    img: './assets/jamie-street-tb5A-QTI6xg-unsplash.jpg',
+  },
+  {
+    name: 'peach',
+    img: './assets/kevin-mueller-KBHqwZ4z69Y-unsplash.jpg',
+  },
+  {
+    name: '1up',
+    img: './assets/mockup-graphics-jHcKq383ibg-unsplash.jpg',
+  },
+  {
+    name: 'mushroom',
+    img: './assets/mockup-graphics-XiWQbLEhFyo-unsplash.jpg',
+  },
+  {
+    name: 'thwomp',
+    img: './assets/onder-ortel-u5cvXg8o4EQ-unsplash.jpg',
+  },
+  {
+    name: 'bulletbill',
+    img: './assets/quaritsch-photography-3HhXWJzG5Ko-unsplash.jpg',
+  },
+  {
+    name: 'coin',
+    img: './assets/quaritsch-photography-JbnUwSe7XuE-unsplash.jpg',
+  },
+  {
+    name: 'goomba',
+    img: './assets/frank-albrecht-5uxgJmZGiVk-unsplash.jpg',
+  },
+]
---
+++ index.js
@@ -0,0 +1,59 @@
+let grid = document.querySelector('.grid')
+let span = document.querySelector('span')
+let allCard = cardsArray.concat(cardsArray)
+
+allCard.sort(() => 0.5 - Math.random())
+let img
+
+// createUI
+allCard.forEach((elm, index) => {
+  let card = document.createElement('div')
+  card.classList.add('div')
+  card.setAttribute('name', elm.name)
+  card.setAttribute('data-id', index)
+  img = document.createElement('img')
+  img.classList.add('none')
+  //   img.classList.add('block')
+
+  img.src = elm.img
+  img.setAttribute('data-id', index)
+  card.append(img)
+  grid.append(card)
+  card.addEventListener('click', handle)
+
+  //  
+})
+
+let arr = []
+let imgSrc = []
+let count = 0
+let countNum = []
+
+// handle
+function handle({ target }) {
+  countNum.push(target)
+  span.innerHTML = countNum.length
+  let img = grid.children[target.dataset.id]
+  imgSrc.push(img.children[0].src)
+  arr.push(target)
+  img.children[0].classList.add('block')
+
+  setTimeout(function () {
+    if (imgSrc[0] === imgSrc[1]) {
+      img.children[0].classList.add('block')
+      arr.splice(0, 2)
+      imgSrc.splice(0, 2)
+    //   console.log(arr)
+      img.children.classList.remove('none')
+    }
+    if (imgSrc[0] !== imgSrc[1] && imgSrc.length == 2) {
+      imgSrc.splice(0, 2)
+    }
+    if (arr.length === 2) {
+      arr.forEach((elm) => {
+        elm.children[0].classList.remove('block')
+      })
+      arr.splice(0, 2)
+    }
+  }, 500)
+}
